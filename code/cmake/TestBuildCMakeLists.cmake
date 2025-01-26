44a36e214e40b46d8b4a812516cb6ff0b9c7a277
Veeti Lukin <veeti.lukin@gmail.com>
2025-01-26 06:50:18 +0200

    Add cmake setup for google tests
    Cmake option for building tests on host platform

---
+++ code/cmake/TestBuildCMakeLists.cmake
@@ -0,0 +1,19 @@
+# ------------------------------ Project setup ------------------------------
+cmake_minimum_required(VERSION 3.20.0)
+
+project(ServoCore LANGUAGES C CXX ASM)
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED TRUE)
+set(CMAKE_C_STANDARD 11)
+
+# Pull in gtest
+include(cmake/gtest_import.cmake)
+
+message(STATUS "C++ compiler: ${CMAKE_CXX_COMPILER}  Version: ${CMAKE_CXX_COMPILER_VERSION}")
+message(STATUS "C compiler: ${CMAKE_C_COMPILER}  Version: ${CMAKE_C_COMPILER_VERSION}")
+#-----------------------------------------------------------------------------
+
+# ------------------------------ Structure setup ------------------------------
+add_subdirectory(libs)
+#-----------------------------------------------------------------------------
