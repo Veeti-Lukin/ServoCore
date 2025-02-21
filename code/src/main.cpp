f4b2fb686a033f75e8431455894fbb8e5493ea0d
Veeti Lukin <veeti.lukin@gmail.com>
2025-02-22 01:06:57 +0200

    Construct some testing parameters in the firmware

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -13,6 +13,8 @@
 #include "drivers/PwmSliceDriver.h"
 #include "led_controller/LedController.h"
 #include "led_controller/common_colors.h"
+#include "parameter_system/ParameterDatabase.h"
+#include "parameter_system/ParameterDelegate.h"
 #include "utils/RingBuffer.h"
 
 #define UART_ID   uart0
@@ -48,6 +50,9 @@ drivers::AnalogRgbLedDriver   led_driver(drivers::AnalogRgbLedType::common_catho
                                          drivers::mapIndexToPwmChannel(pwm_gpio_to_channel(RGB_LED_BLUE_PIN)));
 led_controller::LedController status_led_controller(&led_driver);
 repeating_timer               timer;
+
+parameter_system::ParameterDelegateBase* parameter_buffer[64];
+parameter_system::ParameterDatabase      parameter_database({parameter_buffer});
 
 void uart0_putchar(char c) { uart0_controller.transmitByte(c); }
 
@@ -104,6 +109,102 @@ int main() {
     initSWLibs();
 
     DEBUG_PRINT("Starting up!\n");
+
+    uint8_t  test_uint8  = 42;
+    uint16_t test_uint16 = 1337;
+    uint32_t test_uint32 = 123456;
+    float    test_float  = 3.1415;
+    bool     test_bool   = true;
+
+    parameter_system::ParamUint8  param1(0x2, parameter_system::ReadWriteAccess::read_write, "Test Uint8", test_uint8);
+    parameter_system::ParamUint16 param2(0x3, parameter_system::ReadWriteAccess::read_only, "Test Uint16", test_uint16);
+    parameter_system::ParamUint32 param3(0x4, parameter_system::ReadWriteAccess::read_write, "Test Uint32",
+                                         test_uint32);
+    parameter_system::ParamFloat  param4(0x5, parameter_system::ReadWriteAccess::read_only, "Test Float", test_float);
+    parameter_system::ParamBoolean param5(0x6, parameter_system::ReadWriteAccess::read_write, "Test Bool", test_bool);
+
+    parameter_database.registerParameter(&param1);
+    parameter_database.registerParameter(&param2);
+    parameter_database.registerParameter(&param3);
+    parameter_database.registerParameter(&param4);
+    parameter_database.registerParameter(&param5);
+
+    for (parameter_system::ParameterDelegateBase* parameter : parameter_database.getParameterDelegates()) {
+        const parameter_system::ParameterMetaData* meta_data = parameter->getMetaData();
+        DEBUG_PRINT("ID: % Name: % RW: % TYPE: % ", meta_data->id, meta_data->name,
+                    (uint8_t)meta_data->read_write_access, (uint8_t)meta_data->type);
+        switch (meta_data->type) {
+            case parameter_system::ParameterType::uint8:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::uint8>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::uint16:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::uint16>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::uint32:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::uint32>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::uint64:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::uint32>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::int8:
+                DEBUG_PRINT(
+                    "Value: %",
+                    parameter_database.getParameterDelegateByIdAs<parameter_system::ParameterType::int8>(meta_data->id)
+                        ->getValue());
+                break;
+            case parameter_system::ParameterType::int16:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::int16>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::int32:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::int32>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::int64:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::int64>(meta_data->id)
+                                ->getValue());
+                break;
+            case parameter_system::ParameterType::floating_point:
+                DEBUG_PRINT(
+                    "Value: %",
+                    parameter_database
+                        .getParameterDelegateByIdAs<parameter_system::ParameterType::floating_point>(meta_data->id)
+                        ->getValue());
+                break;
+            case parameter_system::ParameterType::double_float:
+                DEBUG_PRINT("Value: %", parameter_database
+                                            .getParameterDelegateByIdAs<parameter_system::ParameterType::double_float>(
+                                                meta_data->id)
+                                            ->getValue());
+                break;
+            case parameter_system::ParameterType::boolean:
+                DEBUG_PRINT("Value: %",
+                            parameter_database
+                                .getParameterDelegateByIdAs<parameter_system::ParameterType::boolean>(meta_data->id)
+                                ->getValue());
+            default:
+                break;
+        }
+        DEBUG_PRINT("\n");
+    }
 
     while (true) {
         while (uart0_controller.getReceivedBytesAvailableAmount() > 0) {
