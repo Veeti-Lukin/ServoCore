bc969d6449ad00f29b545ed455983b7dec44933f
Veeti Lukin <veeti.lukin@gmail.com>
2025-04-12 14:31:22 +0300

    Add mapping type mapping from cpp type to parameter type and rename type mapping structs

--- code/common/libs/parameter_system/inc/parameter_system/parameter_type_mappings.h
+++ code/common/libs/parameter_system/inc/parameter_system/parameter_type_mappings.h
@@ -1,30 +1,57 @@
 #ifndef PARAMETER_TYPE_MAPPINGS_H
 #define PARAMETER_TYPE_MAPPINGS_H
 
+#include <cstdint>
+
 #include "parameter_system/ParameterDelegate.h"
 
 namespace parameter_system {
 
-// Mapping for each of parameter type to c++ type
+// ****************************************************************************
+// ****** Mapping for each of parameter type to corresponding c++ type ********
+// ****************************************************************************
 
 template <ParameterType type>
-struct ParameterTypeMapping;  // Primary template, left undefined
+struct MapParameterTypeToCppType;  // Primary template, left undefined
 
 // Specializations for each supported type
 // clang-format off
-template <> struct ParameterTypeMapping<ParameterType::uint8>  { using type = uint8_t; };
-template <> struct ParameterTypeMapping<ParameterType::uint16> { using type = uint16_t; };
-template <> struct ParameterTypeMapping<ParameterType::uint32> { using type = uint32_t; };
-template <> struct ParameterTypeMapping<ParameterType::uint64> { using type = uint64_t; };
-template <> struct ParameterTypeMapping<ParameterType::int8>   { using type = int8_t; };
-template <> struct ParameterTypeMapping<ParameterType::int16>  { using type = int16_t; };
-template <> struct ParameterTypeMapping<ParameterType::int32>  { using type = int32_t; };
-template <> struct ParameterTypeMapping<ParameterType::int64>  { using type = int64_t; };
-template <> struct ParameterTypeMapping<ParameterType::floating_point> { using type = float; };
-template <> struct ParameterTypeMapping<ParameterType::double_float>   { using type = double; };
-template <> struct ParameterTypeMapping<ParameterType::boolean>   { using type = bool; };
-// clang-format on
+template <> struct MapParameterTypeToCppType<ParameterType::uint8>  { using type = uint8_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::uint16> { using type = uint16_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::uint32> { using type = uint32_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::uint64> { using type = uint64_t; };
 
+template <> struct MapParameterTypeToCppType<ParameterType::int8>   { using type = int8_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::int16>  { using type = int16_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::int32>  { using type = int32_t; };
+template <> struct MapParameterTypeToCppType<ParameterType::int64>  { using type = int64_t; };
+
+template <> struct MapParameterTypeToCppType<ParameterType::floating_point> { using type = float; };
+template <> struct MapParameterTypeToCppType<ParameterType::double_float>   { using type = double; };
+template <> struct MapParameterTypeToCppType<ParameterType::boolean>        { using type = bool; };
+
+// ****************************************************************************
+// ****** Mapping for each of c++ type to corresponding parameter type ********
+// ****************************************************************************
+
+template <typename T>
+struct MapCppTypeToParameterType;  // Primary template, left undefined
+
+template <> struct MapCppTypeToParameterType<uint8_t>  { static constexpr ParameterType type = ParameterType::uint8; };
+template <> struct MapCppTypeToParameterType<uint16_t> { static constexpr ParameterType type = ParameterType::uint16; };
+template <> struct MapCppTypeToParameterType<uint32_t> { static constexpr ParameterType type = ParameterType::uint32; };
+template <> struct MapCppTypeToParameterType<uint64_t> { static constexpr ParameterType type = ParameterType::uint64; };
+
+template <> struct MapCppTypeToParameterType<int8_t>   { static constexpr ParameterType type = ParameterType::int8; };
+template <> struct MapCppTypeToParameterType<int16_t>  { static constexpr ParameterType type = ParameterType::int16; };
+template <> struct MapCppTypeToParameterType<int32_t>  { static constexpr ParameterType type = ParameterType::int32; };
+template <> struct MapCppTypeToParameterType<int64_t>  { static constexpr ParameterType type = ParameterType::int64; };
+
+template <> struct MapCppTypeToParameterType<float>    { static constexpr ParameterType type = ParameterType::floating_point; };
+template <> struct MapCppTypeToParameterType<double>   { static constexpr ParameterType type = ParameterType::double_float; };
+template <> struct MapCppTypeToParameterType<bool>     { static constexpr ParameterType type = ParameterType::boolean; };
+
+// clang-format on
 }  // namespace parameter_system
 
 #endif  // PARAMETER_TYPE_MAPPINGS_H
