8a4f93fbde193fc7cccfbcae8861144880c20a0c
Veeti Lukin <veeti.lukin@gmail.com>
2025-04-13 21:46:09 +0300

    Wip api read and write

--- code/control_api/template/inc/control_api/Device.h
+++ code/control_api/template/inc/control_api/Device.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 
+#include "parameter_system/ParameterDeclaration.h"
 #include "parameter_system/definitions.h"
 #include "serial_communication_framework/MasterHandler.h"
 #include "utils/StaticList.h"
@@ -12,6 +13,11 @@ namespace servo_core_control_api {
 using ParameterMetaData = parameter_system::ParameterMetaData;
 
 using ParameterID       = parameter_system::ParameterID;
+
+template <typename T>
+concept ParameterDeclaration = requires {
+    { T::param_type };  // The param_type member must exist
+};
 
 // Forward declaration
 class Context;
@@ -27,7 +33,20 @@ public:
 
     utils::StaticList<ParameterID, parameter_system::K_MAX_PARAMETER_ID> getRegisteredParameterIds();
     ParameterMetaData                                                    getParameterMetaData(ParameterID id);
-    bool                                                                 readParameterValue(ParameterID id);
+
+    template <ParameterDeclaration T_Declaration>
+    auto readParameterValue(T_Declaration parameter_declaration) {
+        using CppType = typename parameter_system::MapParameterTypeToCppType<T_Declaration::param_type>::type;
+
+        return CppType();
+    }
+
+    template <ParameterDeclaration T_Declaration>
+    auto writeParameterValue(T_Declaration parameter_declaration) {
+        using CppType = typename parameter_system::MapParameterTypeToCppType<T_Declaration::param_type>::type;
+
+        return CppType();
+    }
 
     static constexpr size_t K_MAX_DEVICE_ID = std::numeric_limits<uint8_t>::max();
 
