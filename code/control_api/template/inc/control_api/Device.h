c6f22fbcb3b265080bbd94237cd4302394e18ab7
Veeti Lukin <veeti.lukin@gmail.com>
2025-04-14 23:16:57 +0300

    Add support in firmware and api to fetch parameter values
    This includes:
    - The protocol request for reading the value
    - Firmware op code handler for the request
    - Logic in api to send the request

--- code/control_api/template/inc/control_api/Device.h
+++ code/control_api/template/inc/control_api/Device.h
@@ -5,6 +5,9 @@
 
 #include "parameter_system/ParameterDeclaration.h"
 #include "parameter_system/definitions.h"
+#include "parameter_system/parameter_type_mappings.h"
+#include "protocol/commands.h"
+#include "protocol/requests.h"
 #include "serial_communication_framework/MasterHandler.h"
 #include "utils/StaticList.h"
 
@@ -12,12 +15,19 @@ namespace servo_core_control_api {
 
 using ParameterMetaData = parameter_system::ParameterMetaData;
 
+using Commands          = protocol::Commands;
 using ParameterID       = parameter_system::ParameterID;
 
 template <typename T>
-concept ParameterDeclaration = requires {
-    { T::param_type };  // The param_type member must exist
+concept ParameterDeclaration = requires(T t) {
+    { T::param_type };  // The param_type static member must exist
+    {
+        t.id
+    };  //-> std::same_as<ParameterID>;  // Non-static member id of type ParameterID //TODO Type checking does nto work
 };
+
+// TODO RENAME EVERY METHOD THAT SENDS SOMETHING TO DEVICE AND WAITS FOR RESULT TO FETCH OR TRYGET INSTEAD OF GET AND
+// SET
 
 // Forward declaration
 class Context;
@@ -36,9 +46,26 @@ public:
 
     template <ParameterDeclaration T_Declaration>
     auto readParameterValue(T_Declaration parameter_declaration) {
-        using CppType = typename parameter_system::MapParameterTypeToCppType<T_Declaration::param_type>::type;
+        using serial_communication_framework::ResponseCode;
+        using serial_communication_framework::ResponseData;
 
-        return CppType();
+        using CppType         = typename parameter_system::MapParameterTypeToCppType<T_Declaration::param_type>::type;
+
+        ResponseData response = communication_handler_->sendRequestAndReceiveResponseBlocking(
+            device_id_, protocol::requests::ReadParameterValue::op_code,
+            protocol::requests::ReadParameterValue::RequestPayload(parameter_declaration.id, T_Declaration::param_type)
+                .serialize());
+
+        if (response.response_code != ResponseCode::ok) {
+            return CppType();
+        }
+
+        protocol::requests::ReadParameterValue::ResponsePayload response_payload(response.response_data);
+
+        CppType value;
+        std::memcpy(&value, response_payload.serialized_value, sizeof(CppType));
+
+        return value;
     }
 
     template <ParameterDeclaration T_Declaration>
