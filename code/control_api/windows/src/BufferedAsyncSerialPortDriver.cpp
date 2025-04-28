6c755d4d8392e80bb1090b045d90f176fe3ee05c
Veeti Lukin <veeti.lukin@gmail.com>
2025-04-28 21:12:32 +0300

    Update windows control api comport driver
    - Add optional debug monitoring of the communication through the driver
    - Clear TX and RX buffer when driver is constructed to remove possible exiting garbage
    - Configure read and write timeouts of windows serial port api
    - Fix readReceivedBytes not reading correct amount of bytes

--- code/control_api/windows/src/BufferedAsyncSerialPortDriver.cpp
+++ code/control_api/windows/src/BufferedAsyncSerialPortDriver.cpp
@@ -2,9 +2,13 @@
 
 #include <format>
 #include <stdexcept>
+#ifdef SERVO_CORE_CONTROL_API_WINDOWS_COMPORT_DRIVER_DEBUG_PRINTS
+#include <iostream>
+#endif
 
 namespace servo_core_control_api::windows::internal {
 
+// TODO RENAME BUFFEREDCOMPORTDRIVER since this is not really async wihtout using overlappedio
 BufferedAsyncSerialPortDriver::BufferedAsyncSerialPortDriver(const char* serial_port_name) {
     /*The first argument to CreateFile is simply the name of the file you want to open. In this
     case, we want to open a serial port, so we use "COM1", "COM2", etc. The next argument tells Windows
@@ -14,7 +18,7 @@ BufferedAsyncSerialPortDriver::BufferedAsyncSerialPortDriver(const char* serial_
     FILE ATTRIBUTE NORMAL, which just tells Windows we don’t want anything fancy here. The final argument
     should also be zero.*/
     serial_port_handle_ =
-        CreateFile(serial_port_name, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+        CreateFile(serial_port_name, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
     if (serial_port_handle_ == INVALID_HANDLE_VALUE) {
         if (GetLastError() == ERROR_FILE_NOT_FOUND) {
@@ -23,10 +27,14 @@ BufferedAsyncSerialPortDriver::BufferedAsyncSerialPortDriver(const char* serial_
         throw std::runtime_error(std::format("could not open serial port: {}", getLastErrorStr()));
     }
 
+    // Purge RX and TX buffers to clear out garbage that might exist in the windows com port buffers
+    PurgeComm(serial_port_handle_, PURGE_RXCLEAR | PURGE_TXCLEAR);
+
     DCB serial_params       = {0};
     serial_params.DCBlength = sizeof(serial_params);
     if (!GetCommState(serial_port_handle_, &serial_params)) {
         // error getting state
+        throw std::runtime_error(std::format("could not get serial parameters: {}", getLastErrorStr()));
     }
     serial_params.BaudRate = CBR_115200;
     serial_params.ByteSize = 8;
@@ -34,7 +42,16 @@ BufferedAsyncSerialPortDriver::BufferedAsyncSerialPortDriver(const char* serial_
     serial_params.Parity   = NOPARITY;
     if (!SetCommState(serial_port_handle_, &serial_params)) {
         // error setting serial port state
+        throw std::runtime_error(std::format("could not set serial parameters: {}", getLastErrorStr()));
     }
+
+    // TODO CALCULATE THESE TIMEOUTS FROM SERIAL_COMMUNICATION_FRAMEWORK TIMEOUTS SO THAT THESE ARE ALWAYS SHORTER?
+    COMMTIMEOUTS timeouts                = {};
+    timeouts.ReadIntervalTimeout         = 25;  // Time between received bytes (ms)
+    timeouts.ReadTotalTimeoutConstant    = 50;  // Total timeout if no bytes received (ms)
+    timeouts.ReadTotalTimeoutMultiplier  = 10;  // Additional time per byte expected
+    timeouts.WriteTotalTimeoutConstant   = 50;
+    timeouts.WriteTotalTimeoutMultiplier = 10;
 }
 
 BufferedAsyncSerialPortDriver::~BufferedAsyncSerialPortDriver() {
@@ -48,12 +65,33 @@ void BufferedAsyncSerialPortDriver::transmitByte(uint8_t byte) {
     if (!WriteFile(serial_port_handle_, &byte, 1, &written, nullptr)) {
         throw std::runtime_error(std::format("could not write to serial port: {}", getLastErrorStr()));
     }
+
+    if (written != 1) {
+        throw std::runtime_error("Serial port writing timed out");
+    }
+
+#ifdef SERVO_CORE_CONTROL_API_WINDOWS_COMPORT_DRIVER_DEBUG_PRINTS
+    std::cout << "Wrote: " << std::hex << static_cast<int>(byte) << std::endl;
+#endif
 }
 
 void BufferedAsyncSerialPortDriver::transmitBytes(std::span<uint8_t> bytes) {
-    for (const uint8_t byte : bytes) {
-        transmitByte(byte);
+    DWORD written;
+    if (!WriteFile(serial_port_handle_, bytes.data(), bytes.size(), &written, nullptr)) {
+        throw std::runtime_error(std::format("could not write to serial port: {}", getLastErrorStr()));
     }
+
+    if (written != bytes.size()) {
+        throw std::runtime_error("Serial port writing timed out");
+    }
+
+#ifdef SERVO_CORE_CONTROL_API_WINDOWS_COMPORT_DRIVER_DEBUG_PRINTS
+    std::cout << "Wrote: ";
+    for (const uint8_t byte : bytes) {
+        std::cout << std::hex << static_cast<int>(byte) << std::endl;
+    }
+    std::cout << std::endl;
+#endif
 }
 
 size_t BufferedAsyncSerialPortDriver::getReceivedBytesAvailableAmount() {
@@ -68,18 +106,41 @@ size_t BufferedAsyncSerialPortDriver::getReceivedBytesAvailableAmount() {
 
 uint8_t BufferedAsyncSerialPortDriver::readReceivedByte() {
     uint8_t byte;
-    DWORD   read;
-    if (!ReadFile(serial_port_handle_, &byte, 1, &read, nullptr)) {
+    DWORD   bytes_read = 0;
+    if (!ReadFile(serial_port_handle_, &byte, 1, &bytes_read, nullptr)) {
         throw std::runtime_error(std::format("could not read from serial port: {}", getLastErrorStr()));
     }
+
+    if (bytes_read != 1) {
+        throw std::runtime_error("Serial port reading timed out");
+    }
+
+#ifdef SERVO_CORE_CONTROL_API_WINDOWS_COMPORT_DRIVER_DEBUG_PRINTS
+    std::cout << "Read: " << std::hex << static_cast<int>(byte) << std::endl;
+#endif
+
     return byte;
 }
 
 size_t BufferedAsyncSerialPortDriver::readReceivedBytes(std::span<uint8_t> buffer) {
-    DWORD bytes_read = 0;
-    if (!ReadFile(serial_port_handle_, buffer.data(), 1, &bytes_read, nullptr)) {
+    DWORD bytes_read    = 0;
+    DWORD bytes_to_read = (buffer.size_bytes() < getReceivedBytesAvailableAmount()) ? buffer.size_bytes()
+                                                                                    : getReceivedBytesAvailableAmount();
+    if (!ReadFile(serial_port_handle_, buffer.data(), bytes_to_read, &bytes_read, nullptr)) {
         throw std::runtime_error(std::format("could not read from serial port: {}", getLastErrorStr()));
     }
+
+    if (bytes_read != bytes_to_read) {
+        throw std::runtime_error("Serial port reading timed out");
+    }
+
+#ifdef SERVO_CORE_CONTROL_API_WINDOWS_COMPORT_DRIVER_DEBUG_PRINTS
+    std::cout << "Read:";
+    for (uint8_t& byte : buffer) {
+        std::cout << static_cast<int>(byte);
+    }
+    std::cout << " " << std::hex << std::endl;
+#endif
 
     return bytes_read;
 }
