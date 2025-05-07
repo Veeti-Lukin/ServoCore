7a4e726362dff134649f6194108b4c99557103e3
Veeti Lukin <veeti.lukin@gmail.com>
2025-05-07 23:52:57 +0300

    Add separate crc for header

--- code/common/libs/serial_communication_framework/inc/serial_communication_framework/packets.h
+++ code/common/libs/serial_communication_framework/inc/serial_communication_framework/packets.h
@@ -12,28 +12,30 @@ struct RequestPacket {
         uint8_t receiver_id    = 0;
         uint8_t operation_code = 0;
         uint8_t payload_size   = 0;
+        uint8_t header_crc     = 0;
     };
 
     Header             header                  = {};
-    uint8_t            crc                     = 0;
+    uint8_t            payload_crc             = 0;
     std::span<uint8_t> payload                 = {};
 
     static constexpr size_t K_PAYLOAD_MAX_SIZE = std::numeric_limits<decltype(Header::payload_size)>::max();
-    static constexpr size_t K_HEADER_SIZE =
-        sizeof(Header::receiver_id) + sizeof(Header::operation_code) + sizeof(Header::payload_size);
-    static constexpr size_t K_HEADER_WITH_CRC_SIZE = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_HEADER_SIZE      = sizeof(Header::receiver_id) + sizeof(Header::operation_code) +
+                                            sizeof(Header::payload_size) + sizeof(Header::header_crc);
+    static constexpr size_t K_HEADER_WITH_CRC_SIZE = K_HEADER_SIZE + sizeof(payload_crc);
 
-    static constexpr size_t K_PACKET_MAX_SIZE      = K_HEADER_SIZE + K_PAYLOAD_MAX_SIZE + sizeof(crc);
-    static constexpr size_t K_PACKET_MIN_SIZE      = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_PACKET_MAX_SIZE      = K_HEADER_SIZE + K_PAYLOAD_MAX_SIZE + sizeof(payload_crc);
+    static constexpr size_t K_PACKET_MIN_SIZE      = K_HEADER_SIZE + sizeof(payload_crc);
 
-    static constexpr size_t K_PAYLOAD_START_OFFSET = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_PAYLOAD_START_OFFSET = K_HEADER_SIZE + sizeof(payload_crc);
 
     RequestPacket()                                = default;
     RequestPacket(uint8_t receiver_id, uint8_t operation_code, std::span<uint8_t> payload)
         : header{.receiver_id    = receiver_id,
                  .operation_code = operation_code,
-                 .payload_size   = static_cast<uint8_t>(payload.size_bytes())},
-          crc(0),
+                 .payload_size   = static_cast<uint8_t>(payload.size_bytes()),
+                 .header_crc     = 0},
+          payload_crc(0),
           payload(payload) {}
 };
 
@@ -41,25 +43,29 @@ struct ResponsePacket {
     struct Header {
         uint8_t response_code = 0;
         uint8_t payload_size  = 0;
+        uint8_t header_crc    = 0;
     };
 
-    Header             header                      = {};
-    uint8_t            crc                         = 0;
-    std::span<uint8_t> payload                     = {};
+    Header             header                  = {};
+    uint8_t            payload_crc             = 0;
+    std::span<uint8_t> payload                 = {};
 
-    static constexpr size_t K_PAYLOAD_MAX_SIZE     = std::numeric_limits<decltype(Header::payload_size)>::max();
-    static constexpr size_t K_HEADER_SIZE          = sizeof(Header::response_code) + sizeof(Header::payload_size);
-    static constexpr size_t K_HEADER_WITH_CRC_SIZE = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_PAYLOAD_MAX_SIZE = std::numeric_limits<decltype(Header::payload_size)>::max();
+    static constexpr size_t K_HEADER_SIZE =
+        sizeof(Header::response_code) + sizeof(Header::payload_size) + sizeof(Header::header_crc);
+    static constexpr size_t K_HEADER_WITH_CRC_SIZE = K_HEADER_SIZE + sizeof(payload_crc);
 
-    static constexpr size_t K_PACKET_MAX_SIZE      = K_HEADER_SIZE + K_PAYLOAD_MAX_SIZE + sizeof(crc);
-    static constexpr size_t K_PACKET_MIN_SIZE      = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_PACKET_MAX_SIZE      = K_HEADER_SIZE + K_PAYLOAD_MAX_SIZE + sizeof(payload_crc);
+    static constexpr size_t K_PACKET_MIN_SIZE      = K_HEADER_SIZE + sizeof(payload_crc);
 
-    static constexpr size_t K_PAYLOAD_START_OFFSET = K_HEADER_SIZE + sizeof(crc);
+    static constexpr size_t K_PAYLOAD_START_OFFSET = K_HEADER_SIZE + sizeof(payload_crc);
 
     ResponsePacket()                               = default;
     ResponsePacket(uint8_t response_code, std::span<uint8_t> payload)
-        : header{.response_code = response_code, .payload_size = static_cast<uint8_t>(payload.size_bytes())},
-          crc(0),
+        : header{.response_code = response_code,
+                 .payload_size  = static_cast<uint8_t>(payload.size_bytes()),
+                 .header_crc    = 0},
+          payload_crc(0),
           payload(payload) {}
 };
 
--- code/common/libs/serial_communication_framework/src/serialize_deserialize.cpp
+++ code/common/libs/serial_communication_framework/src/serialize_deserialize.cpp
@@ -20,13 +20,9 @@ ResponsePacket deSerializeResponse(std::span<uint8_t> data) {
     ASSERT(data.size_bytes() >= ResponsePacket::K_PACKET_MIN_SIZE);
 
     ResponsePacket resp;
-
-    resp.header.response_code = data[0];
-    resp.header.payload_size  = data[1];
-    resp.crc                  = data[2];
+    resp.header = deSerializeResponseHeader(data.subspan(0, ResponsePacket::K_HEADER_SIZE));
 
     ASSERT(data.size_bytes() >= ResponsePacket::K_HEADER_WITH_CRC_SIZE + resp.header.payload_size);
-
     resp.payload = data.subspan(ResponsePacket::K_PAYLOAD_START_OFFSET, resp.header.payload_size);
 
     return resp;
@@ -36,22 +32,27 @@ RequestPacket deSerializeRequest(std::span<uint8_t> data) {
     ASSERT(data.size_bytes() >= RequestPacket::K_PACKET_MIN_SIZE);
 
     RequestPacket req;
-
-    req.header.receiver_id    = data[0];
-    req.header.operation_code = data[1];
-    req.header.payload_size   = data[2];
-    req.crc                   = data[3];
+    req.header = deSerializeRequestHeader(data.subspan(0, RequestPacket::K_HEADER_SIZE));
 
     ASSERT(data.size_bytes() >= RequestPacket::K_HEADER_WITH_CRC_SIZE + req.header.payload_size);
-
     req.payload = data.subspan(RequestPacket::K_PAYLOAD_START_OFFSET, req.header.payload_size);
+
     return req;
 }
 
 std::span<uint8_t> serializeResponse(ResponsePacket resp, std::span<uint8_t> target_buffer) {
+    ASSERT(target_buffer.size_bytes() >= RequestPacket::K_PAYLOAD_MAX_SIZE);
+    // Check that the de serialization still since we are assuming that these values are only one byte long
+    static_assert(sizeof(ResponsePacket::Header::response_code) == 1);
+    static_assert(sizeof(ResponsePacket::Header::payload_size) == 1);
+    static_assert(sizeof(ResponsePacket::Header::header_crc) == 1);
+    static_assert(sizeof(ResponsePacket::payload_crc) == 1);
+
     target_buffer[0]       = resp.header.response_code;
     target_buffer[1]       = resp.header.payload_size;
-    target_buffer[2]       = resp.crc;
+    target_buffer[2]       = resp.header.header_crc;
+
+    target_buffer[3]       = resp.payload_crc;
 
     uint8_t* payload_start = target_buffer.data() + ResponsePacket::K_PAYLOAD_START_OFFSET;
     std::memcpy(payload_start, resp.payload.data(), resp.header.payload_size);
@@ -60,10 +61,20 @@ std::span<uint8_t> serializeResponse(ResponsePacket resp, std::span<uint8_t> tar
 }
 
 std::span<uint8_t> serializeRequest(RequestPacket req, std::span<uint8_t> target_buffer) {
+    ASSERT(target_buffer.size_bytes() >= RequestPacket::K_PAYLOAD_MAX_SIZE);
+    // Check that the de serialization still works since we are assuming that these values are only one byte long
+    static_assert(sizeof(RequestPacket::Header::receiver_id) == 1);
+    static_assert(sizeof(RequestPacket::Header::operation_code) == 1);
+    static_assert(sizeof(RequestPacket::Header::payload_size) == 1);
+    static_assert(sizeof(RequestPacket::Header::header_crc) == 1);
+    static_assert(sizeof(RequestPacket::payload_crc) == 1);
+
     target_buffer[0]       = req.header.receiver_id;
     target_buffer[1]       = req.header.operation_code;
     target_buffer[2]       = req.header.payload_size;
-    target_buffer[3]       = req.crc;
+    target_buffer[3]       = req.header.header_crc;
+
+    target_buffer[4]       = req.payload_crc;
 
     uint8_t* payload_start = target_buffer.data() + RequestPacket::K_PAYLOAD_START_OFFSET;
     std::memcpy(payload_start, req.payload.data(), req.header.payload_size);
@@ -75,8 +86,14 @@ ResponsePacket::Header deSerializeResponseHeader(std::span<uint8_t> data) {
     ASSERT(data.size_bytes() >= ResponsePacket::K_HEADER_SIZE);
 
     ResponsePacket::Header resp_header;
+    // Check that the de serialization still since we are assuming that these values are only one byte long
+    static_assert(sizeof(ResponsePacket::Header::response_code) == 1);
+    static_assert(sizeof(ResponsePacket::Header::payload_size) == 1);
+    static_assert(sizeof(ResponsePacket::Header::header_crc) == 1);
+
     resp_header.response_code = data[0];
     resp_header.payload_size  = data[1];
+    resp_header.header_crc    = data[2];
 
     return resp_header;
 }
@@ -85,9 +102,16 @@ RequestPacket::Header deSerializeRequestHeader(std::span<uint8_t> data) {
     ASSERT(data.size_bytes() >= RequestPacket::K_HEADER_SIZE);
 
     RequestPacket::Header req_header;
+    // Check that the de serialization still works since we are assuming that these values are only one byte long
+    static_assert(sizeof(RequestPacket::Header::receiver_id) == 1);
+    static_assert(sizeof(RequestPacket::Header::operation_code) == 1);
+    static_assert(sizeof(RequestPacket::Header::payload_size) == 1);
+    static_assert(sizeof(RequestPacket::Header::header_crc) == 1);
+
     req_header.receiver_id    = data[0];
     req_header.operation_code = data[1];
     req_header.payload_size   = data[2];
+    req_header.header_crc     = data[3];
 
     return req_header;
 }
