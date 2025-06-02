dd68f14314300f3ed5476d92fa3c200189b7a4f0
Veeti Lukin <veeti.lukin@gmail.com>
2025-06-02 03:58:22 +0300

    Make debug prints inside the control api windows implementation

--- code/control_api/windows/src/Context.cpp
+++ code/control_api/windows/src/Context.cpp
@@ -1,11 +1,20 @@
 #include "control_api/windows/Context.h"
 
+#include <iostream>
+
+#include "debug_print/debug_print.h"
+
 namespace servo_core_control_api::windows {
+
+void debugPrintPutChar(char c) { std::cout << c; }
+void debugPrintFlush() { std::cout << std::flush; }
 
 Context::Context(std::string serial_port_name)
     // serial communication driver must be initialized first before initializing the base class of the context
     : serial_communication_driver_{serial_port_name.c_str()},
-      servo_core_control_api::Context(serial_communication_driver_, program_uptime_clock_) {}
+      servo_core_control_api::Context(serial_communication_driver_, program_uptime_clock_) {
+    debug_print::connectPutCharAndFlushFunctions(debugPrintPutChar, debugPrintFlush);
+}
 
 void Context::open() {
     servo_core_control_api::Context::open();
